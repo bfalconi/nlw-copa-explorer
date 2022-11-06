8abcb2e8dbb454bc063cec742ed57d95f6341eb7
Gabriel Falconi <dgabrielfalconi@gmail.com>
2022-11-06 17:33:34 -0300

    add readme, new folders, change diretory, add grupo G

---
+++ README.md
@@ -0,0 +1,26 @@
+# NLW Copa - Trilha Explorer
+
+![preview](./assets/image/previewgithub.jpg)
+
+Projeto construído no evento Next Level Week (NLW) da Rocketseat.
+
+[📎 Clicando aqui você pode acessar esse projeto!](https://bfalconi.github.io/nlw-copa-explorer)
+
+## 💻 Tecnologias
+
+- HTML
+- CSS
+- JavaScript
+- Git e Github
+
+## 💻 Layout
+
+Você pode visualizar o layout base do projeto através desse [link](https://www.figma.com/file/HrIaQbqencptEBH7yqi5ft/Calend%C3%A1rio-de-Jogos-(Community)?node-id=0%3A1). É necessário ter conta no Figma para acessá-lo.
+
+## 📝 Projeto
+
+É um projeto que apresenta o calendário da Copa do Mundo de 2022 considerando os jogos do Grupo G. A expectativa é adicionar os próximos jogos do Brasil a medida que for avançando de fase. 
+
+## 📩 Contato
+
+dgabrielfalconi@gmail.com
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function createGame(player1, hour, player2) {
     return `
         <li>
-            <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+            <img src="./assets/svg/icon-${player1}.svg" alt="Bandeira do ${player1}">
             <strong>${hour}</strong>
-            <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+            <img src="./assets/svg/icon-${player2}.svg" alt="Bandeira da ${player2}">
         </li>
     `
 }
@@ -23,7 +23,6 @@ function createCard(date, day, games) {
 }
 
 document.querySelector('#cards').innerHTML =    
-createCard("24/11", "quinta", createGame("switzerland", "07:00", "cameroon") + createGame("portugal", "13:00", "cameroon") + createGame("brazil", "16:00", "serbia")) + 
-createCard("28/11", "segunda", createGame("southkorea", "10:00", "cameroon") + createGame("brazil", "13:00", "switzerland") + createGame("portugal", "16:00", "uruguay")) + 
-createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
-    
+createCard("Grupo G - 24/11", "quinta", createGame("switzerland", "07:00", "cameroon") + createGame("brazil", "16:00", "serbia")) + 
+createCard("Grupo G - 28/11", "segunda", createGame("cameroon", "07:00", "serbia") + createGame("brazil", "13:00", "switzerland")) + 
+createCard("Grupo G - 02/12", "sexta", createGame("serbia", "16:00", "switzerland") + createGame("brazil", "16:00", "cameroon"))
